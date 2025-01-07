bdc642c0c91d282730f061ffe3b64aec77238a7a
Pat <peej1818@gmail.com>
2025-01-07 13:35:56 -0500

    Updated mamba .yml. Stubbed out an import of ConfidenceBasedPlanner.

--- adaptive_teaming/planner/__init__.py
+++ adaptive_teaming/planner/__init__.py
@@ -1,6 +1,6 @@
 from .base_planner import (AlwaysHuman, AlwaysLearn, FixedPlanner,
                            InteractionPlanner, LearnThenRobot)
-from .confidence_based_planner import ConfidenceBasedPlanner
+# from .confidence_based_planner import ConfidenceBasedPlanner
 from .facility_location_planner import (FacilityLocationPlanner,
                                         FacilityLocationPrefPlanner,
                                         ConfidenceBasedFacilityLocationPlanner)
